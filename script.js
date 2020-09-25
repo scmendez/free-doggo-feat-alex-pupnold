b801ec02592e877fb1d674a619223d43c4d5aef3
Sandra Mendez <scmendez3b@gmail.com>
2020-09-25 18:43:52 +0200

    add Alex Pup-nold images and movement, moving mountain and boundary function, scrolling sky

---
+++ script.js
@@ -0,0 +1,144 @@
+//belayedAlex: https://www.pinterest.com/pin/66991113186037469/
+//elCap: https://www.yosemitehikes.com/images/wallpaper/yosemitehikes.com-el-cap-west-1920x1200.jpg
+//blueSky: https://i.ytimg.com/vi/0LcLB_JhjxQ/maxresdefault.jpg
+
+//canvas + DOM
+let canvas = document.querySelector('canvas');
+canvas.style.border = '1px solid #000';
+
+//paintbrush
+let ctx = canvas.getContext('2d');
+
+//variables - overall
+let intervalId = 0;
+
+let score = 0;
+
+//variables - drawSky
+let skyBackgroundImg = new Image();
+skyBackgroundImg.src = 'images/skyBackground.jpg';
+let skyY = -skyBackgroundImg.height + canvas.height;
+
+//variables  drawTheCap
+let theCapImg = new Image();
+theCapImg.src = 'images/theCapWall.png';
+let theCapShape = {
+    bottomLeftX: 100,
+    topLeftX: 150,
+    topRightX: 550,
+    bottomRightX: 600,
+}
+
+//variables - alexPupnold
+let alexPupnoldImg = new Image();
+alexPupnoldImg.src = 'images/belayedAlexRight.png';
+let alexPupnoldHeight = 100;
+let alexPupnoldWidth = 100;
+let alexPupnoldX = 300;
+let alexPupnoldY = canvas.height - 95
+let alexPupnoldXIncrement = 1.5;
+
+//variables - moving alexPupnold
+let isRightArrow = false;
+let isLeftArrow = false;
+
+
+
+//classes
+class drawObstacles {
+    constructor(img, x, y) {
+        this.img = img;
+        this.x = x;
+        this.y = y
+    }
+    // draw() {
+
+    // }
+}
+
+
+//event listeners
+document.addEventListener('keydown', (event) => {
+    if (event.key == 'ArrowRight' || event.keyCode == 39) {
+        isRightArrow = true;
+        isLeftArrow = false;
+    } else if (event.key == 'ArrowLeft' || event.keyCode == 37) {
+        isRightArrow = false;
+        isLeftArrow = true;
+    }
+})
+
+document.addEventListener('keyup', (event) => {
+    isRightArrow = false;
+    isLeftArrow = false;
+})
+
+
+//functions
+
+const drawSky = () => {
+    ctx.drawImage(skyBackgroundImg, 0, skyY, canvas.width, skyBackgroundImg.height)
+
+    skyY += .05;
+}
+
+const drawTheCap = () => {
+    let theCapImgPattern = ctx.createPattern(theCapImg, 'repeat');
+
+    ctx.beginPath();
+    ctx.moveTo(theCapShape.bottomLeftX, 500);
+    ctx.lineTo(theCapShape.topLeftX, 0);
+    ctx.lineTo(theCapShape.topRightX, 0);
+    ctx.lineTo(theCapShape.bottomRightX, 500);
+    ctx.lineTo(theCapShape.bottomLeftX, 500);
+
+    ctx.strokeStyle = '#999592';
+    ctx.stroke();
+
+    ctx.fillStyle = theCapImgPattern;
+    ctx.fill();
+
+    ctx.closePath();
+
+    theCapShape.bottomLeftX += .05;
+    theCapShape.topLeftX += .05;
+    theCapShape.topRightX -= .05;
+    theCapShape.bottomRightX -= .05;
+}
+
+const drawAlexPupnold = () => {
+    ctx.drawImage(alexPupnoldImg, alexPupnoldX, alexPupnoldY, alexPupnoldHeight, alexPupnoldWidth)
+}
+
+const moveAlexPupnold = () => {
+    if (isRightArrow) {
+        alexPupnoldX += alexPupnoldXIncrement
+        alexPupnoldImg.src = 'images/belayedAlexRight.png';
+    } else if (isLeftArrow) {
+        alexPupnoldX -= alexPupnoldXIncrement
+        alexPupnoldImg.src = 'images/belayedAlexLeft.png';
+    }
+}
+
+const checkRockBoundaries = () => {
+    if (alexPupnoldX <= theCapShape.bottomLeftX - 35 || ((alexPupnoldX + alexPupnoldWidth) >= theCapShape.bottomRightX + 35)) {
+        clearInterval(intervalId);
+        alert('oh no, you died!')
+    }
+}
+
+const startGame = () => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+
+    drawSky();
+    drawTheCap();
+    drawAlexPupnold();
+    moveAlexPupnold();
+    checkRockBoundaries();
+    //update score
+    //draw obstacles
+}
+
+intervalId = setInterval(() => {
+    requestAnimationFrame(startGame)
+}, 10)
